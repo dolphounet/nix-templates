f64b013d8292ce553b67458befb38c3e1bda5b85
dolphounet <maxencepj.04@gmail.com>
2024-09-23 13:33:24 +0700

    angularls

--- angular/flake.nix
+++ angular/flake.nix
@@ -1,12 +1,18 @@
 {
   inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
     utils.url = "github:numtide/flake-utils";
+    language-servers = {
+      url = "git+https://git.sr.ht/~bwolf/language-servers.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = {
     self,
     nixpkgs,
     utils,
+    language-servers,
   }:
     utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
@@ -16,6 +22,7 @@
           buildInputs = [
             nodejs
             nodePackages."@angular/cli"
+            language-servers.packages.angular-language-server
           ];
         };
     });
